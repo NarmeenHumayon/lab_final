0514ddeb565294e3c7032aa063e6b1c311b9cc0e
NarmeenHumayoun <bscs22013@itu.edu.pk>
2024-12-24 00:14:25 -0700

    Creates new routes for tasks and tasks/new

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login.jsx";
+import NewTask from "./NewTask.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" Component={App} />
+      <Route path="/tasks" Component={App} />
       <Route path="/login" Component={Login} />
+      <Route path="/tasks/new" Component={NewTask} />
     </Routes>
   </BrowserRouter>
 );
