45d07e47bda5dc310f1d4ed4796e6fea00e8ece1
NarmeenHumayoun <bscs22013@itu.edu.pk>
2024-12-24 00:13:55 -0700

    Adds a add new task button

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
 import Card from "../components/Card";
+import NavBar from "../components/NavBar";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -19,13 +20,22 @@ function App() {
   }, []);
   return (
     <>
-      {tasks.map((task) => (
-        <Card
-          title={task.title}
-          description={task.description}
-          dueDate={task.due_date}
-        />
-      ))}
+      <NavBar />
+      <div className="card-cont">
+        {tasks.map((task) => (
+          <Card
+            title={task.title}
+            description={task.description}
+            dueDate={task.due_date}
+          />
+        ))}
+
+        <a className="task-card" href="/tasks/new">
+          <h1 className="task-title hover-op">Add new task +</h1>
+          <p className="task-description"></p>
+          <p className="task-due-date"></p>
+        </a>
+      </div>
     </>
   );
 }
