a7b6bebc425b2390075f0d382af73bc514524da9
NarmeenHumayoun <bscs22013@itu.edu.pk>
2024-12-24 00:45:36 -0700

    Adds routes for task details and edit

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login.jsx";
 import NewTask from "./NewTask.jsx";
+import TaskDetails from "./TaskDetails.jsx";
+import EditTask from "./EditTask.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -13,6 +15,8 @@ createRoot(document.getElementById("root")).render(
       <Route path="/tasks" Component={App} />
       <Route path="/login" Component={Login} />
       <Route path="/tasks/new" Component={NewTask} />
+      <Route path="/tasks/:id" Component={TaskDetails} />
+      <Route path="/tasks/edit/:id" Component={EditTask} />
     </Routes>
   </BrowserRouter>
 );
